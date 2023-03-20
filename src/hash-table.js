0d97932e35e9f962f7c04f7db079641406ef2e2d
Samuel Majerus <ladolego@gmail.com>
2023-03-20 13:52:01 -0700

    Logic, Test 3 -- Pass.   (Lsn 5)

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,4 +7,12 @@ export default class HashTable {
   hash(key) {
     return key.charAt(0).toLowerCase().charCodeAt(0) - 97;
   }
+
+  set(key, value) {  //Adds a key-value pair to the Hash Table 
+    const index = this.hash(key);
+    if (this.array[index] === undefined) {
+      this.array[index] = [];
+    } //Then(?)
+    this.array[index].push([key, value]);
+  }
 }
