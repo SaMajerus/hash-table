4e6cfa318c8804886201cb5b333ef636d20a89da
Samuel Majerus <ladolego@gmail.com>
2023-03-20 13:43:26 -0700

    Logic, Test 2 -- Pass.   (Lsn 5)

--- __tests__/hash-table.test.js
+++ __tests__/hash-table.test.js
@@ -11,4 +11,10 @@ describe('HashTable', () => {
   test('should instantiate a hash with an empty array', () => {
     expect(hashTable.array).toEqual([]);
   });
+
+  /* Hashing Algorithm tests */ 
+  test('should return a number representation of a letter', () => {
+    expect(hashTable.hash("Alaric")).toEqual(0);
+    expect(hashTable.hash("zygorth")).toEqual(25);
+  });
 });
--- src/hash-table.js
+++ src/hash-table.js
@@ -2,4 +2,9 @@ export default class HashTable {
   constructor() {
     this.array = [];
   }
+
+  //Hashing algorithm 'HashTable.prototype.hash'
+  hash(key) {
+    return key.charAt(0).toLowerCase().charCodeAt(0) - 97;
+  }
 }
